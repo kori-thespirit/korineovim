7ed1081c4ca2b4ed82df951ab0137dc1d939ae04
TM-khoa <minhkhoa9a31314@gmail.com>
2026-05-17 19:43:17 +0700

    refactor to korineovim path

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
-require("config.set")
-require("config.keymaps")
-require("config.lazy")
+require("korineovim.config.set")
+require("korineovim.config.keymaps")
+require("korineovim.config.lazy")
 require('mini.pairs').setup()
 require('mini.surround').setup()
 require("mason").setup()
