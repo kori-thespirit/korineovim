b132c7d470ffe24ccf2584a41de49f00006fff82
TM-khoa <minhkhoa9a31314@gmail.com>
2026-06-08 20:44:59 +0700

    feat(clipboard): set unnamedplus for use clipboard

--- init.lua
+++ init.lua
@@ -6,5 +6,6 @@ require("korineovim.lsp")
 require('mini.pairs').setup()
 require('mini.surround').setup()
 require("mason").setup()
+vim.opt.clipboard = "unnamedplus"
 -- require("plugins.persistance").save()
 -- require("plugins.persistance").load()
