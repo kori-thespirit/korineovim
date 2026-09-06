0ca27d79202d5c91c0ed56a75e0c76914c1fb6b4
TM-khoa <minhkhoa9a31314@gmail.com>
2026-09-07 01:02:50 +0700

    feat(fzf,keymap): Add fzf keymap

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 require("korineovim.config.set")
 require("korineovim.config.keymaps")
+require("korineovim.config.fzf_keymaps")
 require("korineovim.config.lazy")
 require("korineovim.lsp")
 require('mini.pairs').setup()
@@ -9,3 +10,4 @@ require("mason").setup()
 vim.opt.clipboard = "unnamedplus"
 -- require("plugins.persistance").save()
 -- require("plugins.persistance").load()
+-- set rtp+=/opt/homebrew/opt/fzf
---
+++ lua/korineovim/config/fzf_keymaps.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>fh", ":FzfLua history<CR>", { desc = 'Show history files' })
+vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { desc = 'Fzf find files' })
